53ef729fee072a0fc6e78916ceee0637fdb7080c
SmDUm <samueldemoraesdelgado2005@gmail.com>
2025-01-22 21:06:13 -0300

    Exercícios

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurante = []
+
 def exibir_nome_do_programa():
 
     print("""
@@ -19,24 +21,47 @@ def exibir_opcoes():
 def finalizar_app():
     os.system('cls')
     print('Finalizando App\n')
+    
+def opcao_invalida():
+    print('Opção Inválida\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    
+def cadastrar_novo_restaurante():
+    
+    os.system('cls')
+    print('Cadastron de novos restaurantes\n')
+    
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurante.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!') 
+    
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main()
 
 def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
-    opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar Restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar Restaurante')
 
-    elif opcao_escolhida == 2:
-        print('Listar Restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar Restaurante')
+            
+        elif opcao_escolhida == 4:
+            finalizar_app()
 
-    elif opcao_escolhida == 3:
-        print('Ativar Restaurante')
-
-    else:
-        finalizar_app()
-
+        else:
+            opcao_invalida()
+            
+    except:
+        opcao_invalida()
+        
 def main():
+        os.system('cls')
         exibir_nome_do_programa()
         exibir_opcoes()
         escolher_opcao()
