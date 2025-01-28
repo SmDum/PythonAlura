116464d580d045dcbaded8efc42f1ebf4eeb6893
SmDUm <samueldemoraesdelgado2005@gmail.com>
2025-01-28 08:25:58 -0300

    Exercícios

---
+++ restaurantes.py
@@ -0,0 +1,13 @@
+class Restaurante:
+    def __init__(self, nome, categoria):
+        self.nome = ''
+        self.categoria = ''
+        self.ativo = False
+    
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+restaurantes = [restaurante_praca, restaurante_pizza]   
+
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
