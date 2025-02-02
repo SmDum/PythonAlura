b3672b2144ac6a282e4c581e6143112896eb93ba
SmDUm <samueldemoraesdelgado2005@gmail.com>
2025-02-02 14:20:48 -0300

    Exercícios

--- Curso2/app.py
+++ Curso2/app.py
@@ -5,13 +5,12 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida("Suco de Melância", 5.0, "Grande")
 prato_paozinho = Prato("Pãozinho", 2.0 , 'O melhor pão da cidade')
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_bebida_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
-
+    restaurante_praca.exibir_cardapio
+    
 if __name__ == '__main__':
     main()
